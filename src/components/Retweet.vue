f9d9c37c54e2d4604cf9467db6aef7f304a9fbb9
taliento <dave@taliento-nb.lan>
2018-11-20 16:23:02 +0100

    retweet text

---
+++ src/components/Retweet.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-card inline style="width: 95%">
+    <q-item>
+      <q-item-main>
+        <q-item-tile label><p v-html="$options.filters.tweetParser(retweet.full_text)"></p></q-item-tile>
+      </q-item-main>
+    </q-item>
+  </q-card>
+</template>
+
+<script>
+export default {
+  name: 'Retweet',
+  props: {
+    retweet: {
+      type: Object
+    }
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
